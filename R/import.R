fff6e261da2de51fbf4dd323de39ec4eb5e6b36c
cortalak <akira.cortal@gmail.com>
2021-01-18 15:22:54 +0100

    namespace cleaning

--- R/import.R
+++ R/import.R
@@ -2,27 +2,26 @@
 #' @import Seurat
 #' @import SingleCellExperiment
 #' @import Matrix
-#' @import matrixStats
-#' @import irlba
 #' @import RcppArmadillo
-#' @import scales
 #' @import data.table
 #' @import scater
 #' @import fgsea
 #' @import ggplot2
 #' @import tictoc
 #' @import BiocParallel
-#' @import stringr
 #' @import fastmatch
+#' @importFrom stringr str_length
+#' @importFrom irlba irlba
 #' @importFrom glue glue
+#' @importFrom matrixStats rowVars
 #' @importFrom pbapply pblapply
 #' @importFrom fastmatch fmatch
 #' @importFrom Rtsne Rtsne
 #' @importFrom umap umap
 #' @importFrom utils menu setTxtProgressBar txtProgressBar
 #' @importFrom stats na.omit p.adjust
-#' @useDynLib CelliD
 #' @importFrom Rcpp sourceCpp
+#' @useDynLib CelliD
 #' @return  updates NAMESPACE import
 import <- function() {
     print("this is a dummy function for roxygen")
